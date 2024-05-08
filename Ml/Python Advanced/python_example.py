f54f4c3ac75d7ba08eec073f44ae474d1c7a1c1e
최세영 <seyoung4503@gmail.com>
2024-05-08 18:25:01 +0900

    add getter & setter

--- Ml/Python Advanced/python_example.py
+++ Ml/Python Advanced/python_example.py
@@ -11,13 +11,29 @@ class Person:
     def print_age(self) -> None:
         return print(self.__age)
     
+    def get_age(self) -> int:
+        return self.__age
+    
+    def set_age(self, age) -> None:
+        if age < 0 :
+            raise ValueError("Invaild age")
+        self.__age = age
+
+    
 
         
 
-person = Person("kim", 20)
+person = Person("kim", 180, 20)
 person.print_age()
 
-person.__age += 1
-person.print_age()
+# not available
+# person.__age += 1
+# person.print_age()
 
+person.set_age(25)
+new_age = person.get_age()
+print(new_age)
+# result : 25
+person.print_age()
+# result : 25
 
